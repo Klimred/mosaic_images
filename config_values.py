3a865efefa90bf85f262c7a260e57e5f8ef79c50
Klimred <fedorov.klimentii@gmail.com>
2024-05-07 23:41:08 +0200

    no big changes

--- config_values.py
+++ config_values.py
@@ -1,11 +1,11 @@
-unresized_images_directory = "./images/unresized 3"
-input_images_directory = "./images/cropped jpgs 3"
+unresized_images_directory = "./images/unresized 2"
+input_images_directory = "./images/cropped jpgs 2"
 target_directory = "./images/target_image/1.jpg"
 
-standard_size = 16
+standard_size = 120
 out_path = "./out"
 
 
 # the original dimensions of the target image were 2048x1152
 dimension_presets = ((128, 72), (256, 144), (512, 288), (1024, 576), (2048, 1152))
-target_dimensions = dimension_presets[2]
+target_dimensions = dimension_presets[1]
